4e0181b60b7a12062ab311bfe99517c4afd4a1ea
Ducky0b <108729478+Ducky0b@users.noreply.github.com>
2025-04-06 09:36:17 +0700

    Update ChoiceCard.js

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -5,7 +5,7 @@ function ChoiceCard({ gameItem, handleGameItemChange }) {
   return (
     <img
       onClick={() => {
-        "Your code here";
+        handelGameItemChange(gameItem);
       }}
       className="choice"
       src={url}
